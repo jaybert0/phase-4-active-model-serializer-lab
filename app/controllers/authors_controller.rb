ae257b36f6726c9f9d415bbbe57f92065a88124b
J Lee <jay.j.lee.1@gmail.com>
2022-02-28 12:29:25 -0500

    completed again

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -3,12 +3,13 @@ class AuthorsController < ApplicationController
 
   def index
     authors = Author.all 
-    render json: authors
+    # render json: authors 
+    render json: authors, include: ["posts.tags"]
   end
 
   def show
     author = Author.find(params[:id])
-    render json: author
+    render json: author, include: ["posts.tags"]
   end
 
   private
